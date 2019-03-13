92b5c95f3665fee28b337d1b77a88b609826d40e
abdulazizF15 <45342163+abdulazizF15@users.noreply.github.com>
2019-03-13 19:19:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` FUCK Off # `,'https://www.twitch.tv/IDK')
+    client.user.setGame(` 5t5Th Fm. `,'https://www.twitch.tv/IDK')
 
 });
 client.login(process.env.bomas);
